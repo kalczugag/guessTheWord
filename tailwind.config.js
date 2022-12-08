f7c2dd6f200c434428c26326d8e364e2a9cf78da
kalczugag <kalczugag@gmail.com>
2022-12-08 19:31:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.ts", "./dist/**/*.js, ./public/**/*/.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
