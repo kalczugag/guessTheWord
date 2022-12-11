abc259afe6012953290e2530a202812964417fce
kalczugag <kalczugag@gmail.com>
2022-12-11 13:32:03 +0100

    working input, word to localStorage and styles

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,10 @@
 "use strict";
-const cards = document.querySelectorAll(".card");
 window.onload = () => {
+    fillCardsFace();
+    const cards = document.querySelectorAll(".card");
     cards.forEach((card) => {
         card.addEventListener("click", () => {
-            card.classList.toggle("is-flipped");
+            card.classList.add("is-flipped");
             let n = card.textContent;
             n = n.replace(/\s+/g, " ");
             // if (n == " A A ") {
@@ -12,7 +13,10 @@ window.onload = () => {
         });
     });
     createLetterCell();
+    guessedLetterInput();
 };
+const cardsContainer = document.querySelector("#cardsContainer");
+const cardFace = document.querySelector(".card__face");
 const lettersContainer = document.querySelector("#chars-container");
 const words = [
     "sowa",
@@ -25,6 +29,19 @@ const words = [
     "oczy",
     "krajobraz",
 ];
+//prettier-ignore
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "X", "Y", "Z",
+];
+const fillCardsFace = () => {
+    for (let i = 0; i < alphabet.length; i++) {
+        cardsContainer.innerHTML += `<div class="scene scene--card">
+                                        <div class="card">
+                                            <div class="card__face card__face--front">${alphabet[i]}</div>
+                                            <div class="card__face card__face--back">${alphabet[i]}</div>
+                                        </div>
+                                    </div>`;
+    }
+};
 const generateRandomWord = () => {
     let random = Math.floor(Math.random() * words.length);
     let randomWord = words[random];
@@ -32,6 +49,9 @@ const generateRandomWord = () => {
 };
 const createLetterCell = () => {
     let word = generateRandomWord();
+    let chars = word.split("");
+    localStorage.setItem("word", JSON.stringify(word));
+    localStorage.setItem("chars", JSON.stringify(chars));
     clearWordCells();
     console.log(word);
     for (let i = 0; i < word.length; i++) {
@@ -48,11 +68,22 @@ const guessedLetterInput = () => {
         const charCode = e.keyCode;
         if ((charCode > 64 && charCode < 91) ||
             (charCode > 96 && charCode < 123)) {
+            compareLetters(e.key);
             return true;
-        }
-        else {
-            return false;
         }
     });
 };
-guessedLetterInput();
+const compareLetters = (input) => {
+    const lettersContainerAsAll = document.querySelectorAll("#chars-container");
+    let wordString = JSON.parse(localStorage.getItem("word") || "");
+    let wordArr = JSON.parse(localStorage.getItem("chars") || "");
+    let wordIndex = -1;
+    for (let i = 0; i < wordArr.length; i++) {
+        if (input == wordArr[i]) {
+            wordIndex = wordArr.indexOf(input);
+        }
+    }
+    //set value on lettersContainer choosen element
+    //for now not working dont know why
+    lettersContainer[wordIndex] = input;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-const cards = document.querySelectorAll(".card");
-
 window.onload = () => {
+    fillCardsFace();
+
+    const cards = document.querySelectorAll(".card");
     cards.forEach((card) => {
         card.addEventListener("click", () => {
-            card.classList.toggle("is-flipped");
+            card.classList.add("is-flipped");
             let n: any = card.textContent;
             n = n.replace(/\s+/g, " ");
             // if (n == " A A ") {
@@ -13,8 +14,11 @@ window.onload = () => {
     });
 
     createLetterCell();
+    guessedLetterInput();
 };
 
+const cardsContainer = document.querySelector("#cardsContainer") as any;
+const cardFace = document.querySelector(".card__face") as HTMLDivElement;
 const lettersContainer = document.querySelector("#chars-container") as any;
 const words: string[] = [
     "sowa",
@@ -27,6 +31,20 @@ const words: string[] = [
     "oczy",
     "krajobraz",
 ];
+//prettier-ignore
+const alphabet: string[] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","W","X","Y","Z",
+];
+
+const fillCardsFace = () => {
+    for (let i = 0; i < alphabet.length; i++) {
+        cardsContainer.innerHTML += `<div class="scene scene--card">
+                                        <div class="card">
+                                            <div class="card__face card__face--front">${alphabet[i]}</div>
+                                            <div class="card__face card__face--back">${alphabet[i]}</div>
+                                        </div>
+                                    </div>`;
+    }
+};
 
 const generateRandomWord = () => {
     let random = Math.floor(Math.random() * words.length);
@@ -36,8 +54,13 @@ const generateRandomWord = () => {
 
 const createLetterCell = () => {
     let word = generateRandomWord();
+    let chars = word.split("");
+    localStorage.setItem("word", JSON.stringify(word));
+    localStorage.setItem("chars", JSON.stringify(chars));
+
     clearWordCells();
     console.log(word);
+
     for (let i = 0; i < word.length; i++) {
         const elem = document.createElement("div");
         elem.classList.add("gss-cell");
@@ -57,11 +80,28 @@ const guessedLetterInput = () => {
             (charCode > 64 && charCode < 91) ||
             (charCode > 96 && charCode < 123)
         ) {
+            compareLetters(e.key);
             return true;
-        } else {
-            return false;
         }
     });
 };
 
-guessedLetterInput();
+const compareLetters = (input: string) => {
+    const lettersContainerAsAll = document.querySelectorAll(
+        "#chars-container"
+    ) as any;
+
+    let wordString = JSON.parse(localStorage.getItem("word") || "") as string;
+    let wordArr = JSON.parse(localStorage.getItem("chars") || "") as string[];
+    let wordIndex = -1;
+
+    for (let i = 0; i < wordArr.length; i++) {
+        if (input == wordArr[i]) {
+            wordIndex = wordArr.indexOf(input);
+        }
+    }
+
+    //set value on lettersContainer choosen element
+    //for now not working dont know why
+    lettersContainer[wordIndex] = input;
+};
