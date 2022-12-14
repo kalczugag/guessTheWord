7402f7f6d03cacb17e37882dfb4f6bdfa427bead
kalczugag <kalczugag@gmail.com>
2022-12-14 19:40:29 +0100

    Release 1.0: no bugs, added connected input

--- dist/main.js
+++ dist/main.js
@@ -12,9 +12,7 @@ window.onload = () => {
             card.classList.add("is-flipped");
             let n = card.textContent;
             n = n.replace(/\s+/g, " ");
-            // if (n == " A A ") {
-            //     console.log(n);
-            // }
+            cardValuesConversion(n);
         });
     });
     trialsLeft.innerHTML = trials.toString();
@@ -31,6 +29,13 @@ const words = [
     "noga",
     "oczy",
     "krajobraz",
+    "kot",
+    "polska",
+    "grzyb",
+    "drut",
+    "salon",
+    "kamyk",
+    "jagoda",
 ];
 //prettier-ignore
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "X", "Y", "Z",
@@ -66,11 +71,196 @@ const createLetterCell = () => {
 const clearWordCells = () => {
     lettersContainer.innerHTML = "";
 };
+const cardValuesConversion = (n) => {
+    let output = "";
+    if (n == " A A ") {
+        output = "";
+        output = "a";
+    }
+    else if (n == " B B ") {
+        output = "";
+        output = "b";
+    }
+    else if (n == " C C ") {
+        output = "";
+        output = "c";
+    }
+    else if (n == " D D ") {
+        output = "";
+        output = "d";
+    }
+    else if (n == " E E ") {
+        output = "";
+        output = "e";
+    }
+    else if (n == " F F ") {
+        output = "";
+        output = "f";
+    }
+    else if (n == " G G ") {
+        output = "";
+        output = "g";
+    }
+    else if (n == " H H ") {
+        output = "";
+        output = "h";
+    }
+    else if (n == " I I ") {
+        output = "";
+        output = "i";
+    }
+    else if (n == " J J ") {
+        output = "";
+        output = "j";
+    }
+    else if (n == " K K ") {
+        output = "";
+        output = "k";
+    }
+    else if (n == " L L ") {
+        output = "";
+        output = "l";
+    }
+    else if (n == " M M ") {
+        output = "";
+        output = "m";
+    }
+    else if (n == " N N ") {
+        output = "";
+        output = "n";
+    }
+    else if (n == " O O ") {
+        output = "";
+        output = "o";
+    }
+    else if (n == " P P ") {
+        output = "";
+        output = "p";
+    }
+    else if (n == " R R ") {
+        output = "";
+        output = "r";
+    }
+    else if (n == " S S ") {
+        output = "";
+        output = "s";
+    }
+    else if (n == " T T ") {
+        output = "";
+        output = "t";
+    }
+    else if (n == " U U ") {
+        output = "";
+        output = "u";
+    }
+    else if (n == " W W ") {
+        output = "";
+        output = "w";
+    }
+    else if (n == " X X ") {
+        output = "";
+        output = "x";
+    }
+    else if (n == " Y Y ") {
+        output = "";
+        output = "y";
+    }
+    else if (n == " Z Z ") {
+        output = "";
+        output = "z";
+    }
+    compareLetters(output);
+};
 const keyboardInputFunction = (e) => {
     const charCode = e.keyCode;
     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
         compareLetters(e.key);
+        flipCardOnKeyboardInput(e.key);
         return true;
+    }
+};
+const flipCardOnKeyboardInput = (input) => {
+    const cardThrd = document.querySelectorAll(".card");
+    const checkIfLetterExistInArray = () => {
+        let otp = false;
+        for (let i = 0; i < wordArr.length; i++) {
+            if (wordArr[i] == input) {
+                otp = true;
+            }
+        }
+        return otp;
+    };
+    if (input == "a" && checkIfLetterExistInArray()) {
+        cardThrd[0].classList.add("is-flipped");
+    }
+    else if (input == "b" && checkIfLetterExistInArray()) {
+        cardThrd[1].classList.add("is-flipped");
+    }
+    else if (input == "c" && checkIfLetterExistInArray()) {
+        cardThrd[2].classList.add("is-flipped");
+    }
+    else if (input == "d" && checkIfLetterExistInArray()) {
+        cardThrd[3].classList.add("is-flipped");
+    }
+    else if (input == "e" && checkIfLetterExistInArray()) {
+        cardThrd[4].classList.add("is-flipped");
+    }
+    else if (input == "f" && checkIfLetterExistInArray()) {
+        cardThrd[5].classList.add("is-flipped");
+    }
+    else if (input == "g" && checkIfLetterExistInArray()) {
+        cardThrd[6].classList.add("is-flipped");
+    }
+    else if (input == "h" && checkIfLetterExistInArray()) {
+        cardThrd[7].classList.add("is-flipped");
+    }
+    else if (input == "i" && checkIfLetterExistInArray()) {
+        cardThrd[8].classList.add("is-flipped");
+    }
+    else if (input == "j" && checkIfLetterExistInArray()) {
+        cardThrd[9].classList.add("is-flipped");
+    }
+    else if (input == "k" && checkIfLetterExistInArray()) {
+        cardThrd[10].classList.add("is-flipped");
+    }
+    else if (input == "l" && checkIfLetterExistInArray()) {
+        cardThrd[11].classList.add("is-flipped");
+    }
+    else if (input == "m" && checkIfLetterExistInArray()) {
+        cardThrd[12].classList.add("is-flipped");
+    }
+    else if (input == "n" && checkIfLetterExistInArray()) {
+        cardThrd[13].classList.add("is-flipped");
+    }
+    else if (input == "o" && checkIfLetterExistInArray()) {
+        cardThrd[14].classList.add("is-flipped");
+    }
+    else if (input == "p" && checkIfLetterExistInArray()) {
+        cardThrd[15].classList.add("is-flipped");
+    }
+    else if (input == "r" && checkIfLetterExistInArray()) {
+        cardThrd[16].classList.add("is-flipped");
+    }
+    else if (input == "s" && checkIfLetterExistInArray()) {
+        cardThrd[17].classList.add("is-flipped");
+    }
+    else if (input == "t" && checkIfLetterExistInArray()) {
+        cardThrd[18].classList.add("is-flipped");
+    }
+    else if (input == "u" && checkIfLetterExistInArray()) {
+        cardThrd[19].classList.add("is-flipped");
+    }
+    else if (input == "w" && checkIfLetterExistInArray()) {
+        cardThrd[20].classList.add("is-flipped");
+    }
+    else if (input == "x" && checkIfLetterExistInArray()) {
+        cardThrd[21].classList.add("is-flipped");
+    }
+    else if (input == "y" && checkIfLetterExistInArray()) {
+        cardThrd[22].classList.add("is-flipped");
+    }
+    else if (input == "z" && checkIfLetterExistInArray()) {
+        cardThrd[23].classList.add("is-flipped");
     }
 };
 const guessedLetterInput = () => {
@@ -98,6 +288,9 @@ const compareLetters = (input) => {
         }
         else {
             resultDiv.innerHTML = "Skończyły się próby. Przegrałeś!";
+            for (let i = 0; i < wordArr.length; i++) {
+                lettersContainerAsAll[i].innerHTML = wordArr[i];
+            }
             deleteEventListeners();
             restartGame(2000);
         }
--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,7 @@ window.onload = () => {
             card.classList.add("is-flipped");
             let n: any = card.textContent;
             n = n.replace(/\s+/g, " ");
-            // if (n == " A A ") {
-            //     console.log(n);
-            // }
+            cardValuesConversion(n);
         });
     });
 
@@ -35,6 +33,13 @@ const words: string[] = [
     "noga",
     "oczy",
     "krajobraz",
+    "kot",
+    "polska",
+    "grzyb",
+    "drut",
+    "salon",
+    "kamyk",
+    "jagoda",
 ];
 //prettier-ignore
 const alphabet: string[] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","W","X","Y","Z",
@@ -51,7 +56,7 @@ const fillCardsFace = () => {
     }
 };
 
-const generateRandomWord = () => {
+const generateRandomWord = (): string => {
     let random = Math.floor(Math.random() * words.length);
     let randomWord = words[random];
     return randomWord;
@@ -64,7 +69,6 @@ const createLetterCell = () => {
     let word = generateRandomWord();
     let chars = word.split("");
     wordArr = chars;
-
     clearWordCells();
 
     for (let i = 0; i < word.length; i++) {
@@ -78,12 +82,157 @@ const clearWordCells = () => {
     lettersContainer.innerHTML = "";
 };
 
+const cardValuesConversion = (n: string) => {
+    let output = "";
+
+    if (n == " A A ") {
+        output = "";
+        output = "a";
+    } else if (n == " B B ") {
+        output = "";
+        output = "b";
+    } else if (n == " C C ") {
+        output = "";
+        output = "c";
+    } else if (n == " D D ") {
+        output = "";
+        output = "d";
+    } else if (n == " E E ") {
+        output = "";
+        output = "e";
+    } else if (n == " F F ") {
+        output = "";
+        output = "f";
+    } else if (n == " G G ") {
+        output = "";
+        output = "g";
+    } else if (n == " H H ") {
+        output = "";
+        output = "h";
+    } else if (n == " I I ") {
+        output = "";
+        output = "i";
+    } else if (n == " J J ") {
+        output = "";
+        output = "j";
+    } else if (n == " K K ") {
+        output = "";
+        output = "k";
+    } else if (n == " L L ") {
+        output = "";
+        output = "l";
+    } else if (n == " M M ") {
+        output = "";
+        output = "m";
+    } else if (n == " N N ") {
+        output = "";
+        output = "n";
+    } else if (n == " O O ") {
+        output = "";
+        output = "o";
+    } else if (n == " P P ") {
+        output = "";
+        output = "p";
+    } else if (n == " R R ") {
+        output = "";
+        output = "r";
+    } else if (n == " S S ") {
+        output = "";
+        output = "s";
+    } else if (n == " T T ") {
+        output = "";
+        output = "t";
+    } else if (n == " U U ") {
+        output = "";
+        output = "u";
+    } else if (n == " W W ") {
+        output = "";
+        output = "w";
+    } else if (n == " X X ") {
+        output = "";
+        output = "x";
+    } else if (n == " Y Y ") {
+        output = "";
+        output = "y";
+    } else if (n == " Z Z ") {
+        output = "";
+        output = "z";
+    }
+
+    compareLetters(output);
+};
+
 const keyboardInputFunction = (e: KeyboardEvent) => {
     const charCode = e.keyCode;
 
     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)) {
         compareLetters(e.key);
+        flipCardOnKeyboardInput(e.key);
         return true;
+    }
+};
+
+const flipCardOnKeyboardInput = (input: string) => {
+    const cardThrd = document.querySelectorAll(".card");
+
+    const checkIfLetterExistInArray = (): boolean => {
+        let otp = false;
+        for (let i = 0; i < wordArr.length; i++) {
+            if (wordArr[i] == input) {
+                otp = true;
+            }
+        }
+        return otp;
+    };
+
+    if (input == "a" && checkIfLetterExistInArray()) {
+        cardThrd[0].classList.add("is-flipped");
+    } else if (input == "b" && checkIfLetterExistInArray()) {
+        cardThrd[1].classList.add("is-flipped");
+    } else if (input == "c" && checkIfLetterExistInArray()) {
+        cardThrd[2].classList.add("is-flipped");
+    } else if (input == "d" && checkIfLetterExistInArray()) {
+        cardThrd[3].classList.add("is-flipped");
+    } else if (input == "e" && checkIfLetterExistInArray()) {
+        cardThrd[4].classList.add("is-flipped");
+    } else if (input == "f" && checkIfLetterExistInArray()) {
+        cardThrd[5].classList.add("is-flipped");
+    } else if (input == "g" && checkIfLetterExistInArray()) {
+        cardThrd[6].classList.add("is-flipped");
+    } else if (input == "h" && checkIfLetterExistInArray()) {
+        cardThrd[7].classList.add("is-flipped");
+    } else if (input == "i" && checkIfLetterExistInArray()) {
+        cardThrd[8].classList.add("is-flipped");
+    } else if (input == "j" && checkIfLetterExistInArray()) {
+        cardThrd[9].classList.add("is-flipped");
+    } else if (input == "k" && checkIfLetterExistInArray()) {
+        cardThrd[10].classList.add("is-flipped");
+    } else if (input == "l" && checkIfLetterExistInArray()) {
+        cardThrd[11].classList.add("is-flipped");
+    } else if (input == "m" && checkIfLetterExistInArray()) {
+        cardThrd[12].classList.add("is-flipped");
+    } else if (input == "n" && checkIfLetterExistInArray()) {
+        cardThrd[13].classList.add("is-flipped");
+    } else if (input == "o" && checkIfLetterExistInArray()) {
+        cardThrd[14].classList.add("is-flipped");
+    } else if (input == "p" && checkIfLetterExistInArray()) {
+        cardThrd[15].classList.add("is-flipped");
+    } else if (input == "r" && checkIfLetterExistInArray()) {
+        cardThrd[16].classList.add("is-flipped");
+    } else if (input == "s" && checkIfLetterExistInArray()) {
+        cardThrd[17].classList.add("is-flipped");
+    } else if (input == "t" && checkIfLetterExistInArray()) {
+        cardThrd[18].classList.add("is-flipped");
+    } else if (input == "u" && checkIfLetterExistInArray()) {
+        cardThrd[19].classList.add("is-flipped");
+    } else if (input == "w" && checkIfLetterExistInArray()) {
+        cardThrd[20].classList.add("is-flipped");
+    } else if (input == "x" && checkIfLetterExistInArray()) {
+        cardThrd[21].classList.add("is-flipped");
+    } else if (input == "y" && checkIfLetterExistInArray()) {
+        cardThrd[22].classList.add("is-flipped");
+    } else if (input == "z" && checkIfLetterExistInArray()) {
+        cardThrd[23].classList.add("is-flipped");
     }
 };
 
@@ -121,6 +270,11 @@ const compareLetters = (input: string) => {
             trialsLeft.innerHTML = trials.toString();
         } else {
             resultDiv.innerHTML = "Skończyły się próby. Przegrałeś!";
+
+            for (let i = 0; i < wordArr.length; i++) {
+                lettersContainerAsAll[i].innerHTML = wordArr[i];
+            }
+
             deleteEventListeners();
             restartGame(2000);
         }
